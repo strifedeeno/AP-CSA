6b1311850d936418726286d63ae0ac110d7977ad
strifedeeno <coolarnay@gmail.com>
2023-08-12 15:32:51 +0000

    12 Aug 2023, 08:32

--- Lesson 1.2/Lesson1.2/src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args){
-        System.out.println("Hello, World!");
-        System.out.println("Hello who are you?");
-        System.out.println("Testing");
-        System.out.println("HOw  is this happening");
-        System.out.println("Testing on chromebook!");
-    }
-}
