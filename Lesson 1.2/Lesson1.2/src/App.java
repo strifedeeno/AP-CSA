ce4b744f88cec2ec6109c847d88988a9b8c942ff
strifedeeno <coolarnay@gmail.com>
2023-08-12 06:17:51 +0000

    11 Aug 2023, 23:17

--- Lesson 1.2/Lesson1.2/src/App.java
+++ Lesson 1.2/Lesson1.2/src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("Hello who are you?")
+        System.out.println("Hello who are you?");
+        System.out.println("Testing");
     }
 }
