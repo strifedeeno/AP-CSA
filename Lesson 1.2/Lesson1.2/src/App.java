5ee8310b7c8314ea56cd72257cd40a9b8c7a20c5
strifedeeno <coolarnay@gmail.com>
2023-08-12 06:27:23 +0000

    11 Aug 2023, 23:27

--- Lesson 1.2/Lesson1.2/src/App.java
+++ Lesson 1.2/Lesson1.2/src/App.java
@@ -3,5 +3,6 @@ public class App {
         System.out.println("Hello, World!");
         System.out.println("Hello who are you?");
         System.out.println("Testing");
+        System.out.println("HO");
     }
 }
