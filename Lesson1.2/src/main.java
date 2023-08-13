6140e43bac51d44d38cbf72b096bcf1cf2dfd993
strifedeeno <coolarnay@gmail.com>
2023-08-13 08:02:47 +0000

    13 August 2023 at 1:02:47 GMT-7

--- Lesson1.2/src/main.java
+++ Lesson1.2/src/main.java
@@ -2,8 +2,9 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("How are you??");
+        doge("Doggy");
     }
     public static void doge(String dogename){
-        System.out.println("The dogename is" );
+        System.out.println("The dogename is " + dogename);
     }
 }
