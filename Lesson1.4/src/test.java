03f127930069f2ec2275d22537d48f83b14f6f00
strifedeeno <coolarnay@gmail.com>
2023-08-16 21:14:15 +0000

    16 August 2023 at 14:14:15 GMT-7

--- Lesson1.4/src/test.java
+++ Lesson1.4/src/test.java
@@ -1,6 +1,15 @@
-import java.rmi.Remote;
-
 public class test {
-    Remote repository contains conflicting changes
+    public static void main(String[] args) {
+        System.out.println("some lines of code");
+        System.out.println("have // characters on them");
+        System.out.println("which means ");  // that they are comments
+        // System.out.println("written by the programmer.");
+
+        System.out.println("lines can also");
+        System.out.println("have /* and */ characters");
+        /* System.out.println("which represents");
+        System.out.println("a multi-line style");
+        */ System.out.println("of comment.");
+    }
     
 }
