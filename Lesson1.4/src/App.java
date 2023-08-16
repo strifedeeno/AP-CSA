90f5de1e3f03ed928defa344e01eea17cebd0457
strifedeeno <coolarnay@gmail.com>
2023-08-16 20:48:00 +0000

    16 August 2023 at 13:48:00 GMT-7

--- Lesson1.4/src/App.java
+++ Lesson1.4/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args){
         //This is a comment.
-        /*U */
+        /*Use camel  */
         System.out.println("Hello, World!");
     }
 }
