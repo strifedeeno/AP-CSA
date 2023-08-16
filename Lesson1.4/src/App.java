ab14ee1428e2135496c66896ae328d21a0bb882a
strifedeeno <coolarnay@gmail.com>
2023-08-16 21:07:56 +0000

    16 August 2023 at 14:07:56 GMT-7

--- Lesson1.4/src/App.java
+++ Lesson1.4/src/App.java
@@ -2,14 +2,14 @@ public class App {
     public static void main(String[] args){
         System.out.println("""
                      @ @ @ @ @ @ @
-                    @ @ @ @ @ @ @ @ @ @
+                   @ @ @ @ @ @ @ @ @ @
                   @ @ @ @ @ @ @ @ @ @ @
-                 |||||||||[""""]||||||||
+                 |||||||||[""\"\"]||||||||
                 |||||||||[......]||||||||
                     OOOOOOOOOOOOOOOOO
                      OOOOOOOOOOOOOOO
                         OOOOOOOOOO
-                        OOOOOOOO
+                         OOOOOOOO
 
                 """);
     }
