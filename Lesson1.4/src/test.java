998ae86d1c579858490f42991eb4d1e0d4f6dc8d
strifedeeno <coolarnay@gmail.com>
2023-08-16 21:14:36 +0000

    16 August 2023 at 14:14:36 GMT-7

--- Lesson1.4/src/test.java
+++ Lesson1.4/src/test.java
@@ -1,15 +1,4 @@
 public class test {
-    public static void main(String[] args) {
-        System.out.println("some lines of code");
-        System.out.println("have // characters on them");
-        System.out.println("which means ");  // that they are comments
-        // System.out.println("written by the programmer.");
-
-        System.out.println("lines can also");
-        System.out.println("have /* and */ characters");
-        /* System.out.println("which represents");
-        System.out.println("a multi-line style");
-        */ System.out.println("of comment.");
-    }
+    
     
 }
