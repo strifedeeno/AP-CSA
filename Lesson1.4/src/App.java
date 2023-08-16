fadbf0f8e8d85b6a22f6ec19b5471677952e01c1
strifedeeno <coolarnay@gmail.com>
2023-08-16 21:00:48 +0000

    16 August 2023 at 14:00:48 GMT-7

--- Lesson1.4/src/App.java
+++ Lesson1.4/src/App.java
@@ -7,7 +7,9 @@ public class App {
                 |||||||||[\"\"\"\"]||||||||
                 |||||||||[.......]||||||||
                    OOOOOOOOOOOOOOOOO
-                   
+                   OOOOOOOOOOOOOOO
+                   OOOOOOOOOO
+                   OOOOOOOO
 
                 """);
     }
