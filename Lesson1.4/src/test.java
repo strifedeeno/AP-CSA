b072504d88ac1a1ed3f33ed4c412ba175882bec4
strifedeeno <coolarnay@gmail.com>
2023-08-17 05:04:28 +0000

    16 August 2023 at 22:04:28 GMT-7

--- Lesson1.4/src/test.java
+++ Lesson1.4/src/test.java
@@ -1,17 +1,5 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("""
-            //////////////////////
-            || Victory is mine! ||
-            \\\\\\\\\\\\\\\\\\\\\\
-            || Victory is mine! ||
-            \\\\\\\\\\\\\\\\\\\\\\
-            || Victory is mine! ||
-            \\\\\\\\\\\\\\\\\\\\\\
-            || Victory is mine! ||
-            \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-            || Victory is mine! ||
-            \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-                """);
+        System.out.println("""""";);
     }
 }
