1bce337377262cbb836d043a0a0698e873ba7ad2
strifedeeno <coolarnay@gmail.com>
2023-08-23 21:13:51 +0000

    Invalid DateTime

--- 2.04 worksheet/src/App.java
+++ 2.04 worksheet/src/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println(4.0/3.0);
         System.out.println("How are you.");
-        System.out.println(1 + 1 + 1 + 1 + 1 + "1" + 1 + 1 + 1 + 1);
-        
+        System.out.println((1 + 1 + 1 + 1 + 1 + 1 + 1) / 8 + "oops");
+
     }
 }
