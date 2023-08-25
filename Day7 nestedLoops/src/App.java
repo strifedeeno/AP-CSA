93019a2219a8e6c00c6bee71eb29f11bc0030ad7
strifedeeno <coolarnay@gmail.com>
2023-08-25 20:53:54 +0000

    Invalid DateTime

--- Day7 nestedLoops/src/App.java
+++ Day7 nestedLoops/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         for (int i = 1;i<=5;i++){
-            for (int j = 5;j>0;j--){
+            for (int j = 5;j>0;--j){
                 System.out.print(i);
             }
             System.out.println();
