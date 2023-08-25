f2816115b043727fa3f9eae291ba25df2e173787
strifedeeno <coolarnay@gmail.com>
2023-08-25 20:42:00 +0000

    Invalid DateTime

--- 2.04 worksheet/src/App.java
+++ 2.04 worksheet/src/App.java
@@ -1,8 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println(4.0/3.0);
-        System.out.println("How are you.");
-        System.out.println((1 + 1 + 1 + 1 + 1 + 1 + 1) / 8 + "oops");
+        System.out.println((int) (1.8 * 2.0));
+        System.out.println((int) 1.8 * 2.0);
+        System.out.println((double) 3 / 4);
+        System.out.println(3 / (double) 4);
+        System.out.println((1 + 1 + 1 + 1 + (double)1 + 1 + 1) / 8);
+        System.out.println((int) (1 + 1 + 1 + 1 + 1.0 + 1 + 1) / 8);
+        System.out.println("I give this worksheet " + (int) 5.1 + " out of five stars.");
+        System.out.println("integer 3 / 4 is " + (3 / 4) + " but double 3 / 4 is " + (double)3 / 4);
 
     }
 }
