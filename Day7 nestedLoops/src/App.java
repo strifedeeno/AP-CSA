035b46125c5dd7b6d7ce50d103bff9536c38608f
strifedeeno <coolarnay@gmail.com>
2023-08-25 21:54:32 +0000

    Invalid DateTime

--- Day7 nestedLoops/src/App.java
+++ Day7 nestedLoops/src/App.java
@@ -12,12 +12,13 @@ public class App {
         System.out.println("b is " + b);
     }
     public static void anotherFancyMethod() {
-        /* */
+        /*
         int a = 1;
         for (int i = 0; i < 3; i++) {
             int e = 42;
             System.out.println("e is " + e);
         }
         System.out.println("a is " + a);
+        */
     }
 }
