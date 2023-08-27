387f192e6896290a297722f8e9c8a9d9e581def9
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-08-27 05:51:25 +0000

    Invalid DateTime

--- Day7 nestedLoops/src/App.java
+++ Day7 nestedLoops/src/App.java
@@ -2,7 +2,6 @@ public class App {
     public static void fancyMethod() {
         int d = 7;
         System.out.println("d is " + d);
->>>>>>> e2f60996bd1528f112247bd18ddf969f938131b4
     }
     public static void main(String[] args) {
         int c = 10; 
