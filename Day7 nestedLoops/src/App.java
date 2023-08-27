6906530a1af14ed4a4caea16deb9fecdb4caf39a
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-08-26 22:51:05 -0700

    something

--- Day7 nestedLoops/src/App.java
+++ Day7 nestedLoops/src/App.java
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("test");
-=======
 public class App { 
     public static void fancyMethod() {
         int d = 7;
