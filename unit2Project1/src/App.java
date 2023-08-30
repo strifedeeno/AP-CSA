2370e53cd104e6c68db848825a2b0cf7e9f6b954
strifedeeno <coolarnay@gmail.com>
2023-08-30 21:48:43 +0000

    Invalid DateTime

--- unit2Project1/src/App.java
+++ unit2Project1/src/App.java
@@ -4,7 +4,7 @@ public class App {
        System.out.println("Main");
        bracketQuoteThing();
        for(int i=0;i<NUM_LEVELS;i++){
-        System.out.print("\\");
+        System.out.print("\\\\");
        }
        middleThing();
        bracketQuoteThing();
