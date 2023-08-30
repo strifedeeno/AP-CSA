b0ff28d14cade79e415c597ed246ae78bb3da514
strifedeeno <coolarnay@gmail.com>
2023-08-30 21:33:42 +0000

    Invalid DateTime

--- unit2Project1/src/App.java
+++ unit2Project1/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static int NUM_LEVELS=4;
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
     }
 }
