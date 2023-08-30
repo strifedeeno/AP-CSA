ed99ccc5b02b86293e1a00c515f422f2dfcff421
strifedeeno <coolarnay@gmail.com>
2023-08-30 21:43:10 +0000

    Invalid DateTime

--- unit2Project1/src/App.java
+++ unit2Project1/src/App.java
@@ -2,6 +2,9 @@ public class App {
     public static int NUM_LEVELS=4;
     public static void main(String[] args) throws Exception {
        System.out.println("Main");
+       bracketQuoteThing();
     }
-    public 
+    public static void bracketQuoteThing(){
+        System.out.println("|\"\"\"\"\"\"\"\"\"\"|");
+    }
 }
