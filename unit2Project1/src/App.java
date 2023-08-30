8e42525f389b5451212260a9c0d96668ef6b089b
strifedeeno <coolarnay@gmail.com>
2023-08-30 21:34:47 +0000

    Invalid DateTime

--- unit2Project1/src/App.java
+++ unit2Project1/src/App.java
@@ -2,8 +2,8 @@ public class App {
     public static int NUM_LEVELS=4;
     public static void main(String[] args) throws Exception {
         int rowlevel=0;
-        if (rowlevel==0){
-            
+        if (rowlevel==0 ){
+
         }
     }
 }
