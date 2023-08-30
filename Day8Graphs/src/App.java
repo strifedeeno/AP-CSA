fb5084d5d4c3064060b6202b030da74fc8489360
strifedeeno <coolarnay@gmail.com>
2023-08-30 21:02:01 +0000

    Invalid DateTime

--- Day8Graphs/src/App.java
+++ Day8Graphs/src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         for(int i =1;i<7;i++){
+            f
             for(int j=0; j<i; j++){
                 System.out.print(i);
             }
