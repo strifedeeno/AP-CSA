23d2405b88d67de6b07490580d0512ac3f7b9c5d
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-08-31 04:24:02 +0000

    Invalid DateTime

---
+++ test/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        for(int x = 1; x<3;x++){
+            for (int y = 2; y<5;y++){
+                for(int z = 3; z<4;z++){
+                    System.out.print(y);}}}
+
+
+
+
+
+
+
+    }}
