e48fea2f0f42f56f4991a3de93f8c03b9904c999
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-08-31 05:13:18 +0000

    Invalid DateTime

--- unit2Project1/src/App.java
+++ unit2Project1/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static int NUM_LEVELS=5;
     public static void main(String[] args) throws Exception {
-        int lengthOfthing = NUM_LEVELS*2+2;
+        int lengthOfthing = NUM_LEVELS*2;
         System.out.println(lengthOfthing);
        System.out.println("Main");
        bracketQuoteThing();
