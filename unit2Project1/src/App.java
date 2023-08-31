f549679d27a63dd1741752d31e67615d5a875669
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-08-31 05:26:37 +0000

    Invalid DateTime

--- unit2Project1/src/App.java
+++ unit2Project1/src/App.java
@@ -39,6 +39,6 @@ public class App {
             System.out.print(" ");
         }
         System.out.println("||");
-        System.out.println("tl");
+        System.out.println("tld");
     }
 }
