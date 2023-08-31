3b8ee1ed68c054f7b672d9a58019e32654930d99
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-08-31 05:20:12 +0000

    Invalid DateTime

--- unit2Project1/src/App.java
+++ unit2Project1/src/App.java
@@ -1,12 +1,10 @@
 public class App {
-    public static int NUM_LEVELS=5;
+    public static int NUM_LEVELS=200;
     public static void main(String[] args) throws Exception {
         int lengthOfthing = NUM_LEVELS*2;
-        System.out.println(lengthOfthing);
-       System.out.println("Main");
        bracketQuoteThing();
        for(int i=NUM_LEVELS;i>0;i--){
-        for(int x=lengthOfthing-1;0<=x-i*2;x-=2){
+        for(int x=lengthOfthing;0<=x-i*2;x-=2){
             System.out.print(" ");
         }
         System.out.print("\\");
@@ -16,8 +14,8 @@ public class App {
         System.out.println("/");
        }
        middleThing();
-       for(int i=1;i<NUM_LEVELS;i++){
-         for(int x=lengthOfthing-1;0<=x-i*2;x-=2){
+       for(int i=1;i<=NUM_LEVELS;i++){
+         for(int x=lengthOfthing;0<=x-i*2;x-=2){
             System.out.print(" ");
         }
         System.out.print("/");
@@ -30,14 +28,14 @@ public class App {
     }
     public static void bracketQuoteThing(){
         System.out.print("|");
-        for(int quotes= 0; quotes<NUM_LEVELS*2;quotes++){
+        for(int quotes= -2; quotes<NUM_LEVELS*2;quotes++){
             System.out.print("\"");
         }
         System.out.print("|");
         System.out.println();
     }
     public static void middleThing(){
-         for(int x=NUM_LEVELS-1;x>=0;x--){
+         for(int x=NUM_LEVELS;x>=0;x--){
             System.out.print(" ");
         }
         System.out.println("||");
