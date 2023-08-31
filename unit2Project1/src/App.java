48883b729945e9cef378d35d868fa8f0551a8486
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-08-31 04:47:15 +0000

    Invalid DateTime

--- unit2Project1/src/App.java
+++ unit2Project1/src/App.java
@@ -25,7 +25,7 @@ public class App {
         for(int quotes= 0; quotes<NUM_LEVELS*2;quotes++){
             System.out.print("\"");
         }
-        System.out.print(" |");
+        System.out.print("|");
     }
     public static void middleThing(){
         System.out.println("||");
