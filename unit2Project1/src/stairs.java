9b421f4db5adfed7979dacd2cd7789ac129a73ab
strifedeeno <coolarnay@gmail.com>
2023-09-01 15:46:50 +0000

    Friday, September 1, 2023 at 8:46:50 AM Pacific Daylight Time

--- unit2Project1/src/stairs.java
+++ unit2Project1/src/stairs.java
@@ -1,6 +1,6 @@
 public class stairs {
-    public static int Stairs=5;
+    public static int Stairs=6;
     public static void main(String[] args){
-        System.out.println("Hello");
+        System.out.print
     }
 }
