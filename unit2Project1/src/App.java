7f9db8ff0eb4489852c70d991ab294d4431d53b6
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-09-02 04:34:53 +0000

    Friday, September 1, 2023 at 9:34:53 PM Pacific Daylight Time

--- unit2Project1/src/App.java
+++ unit2Project1/src/App.java
@@ -35,7 +35,7 @@ public class App {
         System.out.println();
     }
     public static void middleThing(){
-         for(int x=NUM_LEVELS;x>=0;x--){
+         for(int x=NUM_LEVELS;x>=2;x--){
             System.out.print(" ");
         }
     }
