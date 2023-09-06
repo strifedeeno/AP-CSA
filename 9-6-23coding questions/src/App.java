9f11f40029c9d370748131c6b1e7e094284f4931
strifedeeno <coolarnay@gmail.com>
2023-09-06 21:15:54 +0000

    Wednesday, September 6, 2023 at 2:15:54 PM Pacific Daylight Time

--- 9-6-23coding questions/src/App.java
+++ 9-6-23coding questions/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         for(int x=0;x<=10;x++){
-            for()
+            for(int y=6;y>=0;y--){
             if (x==0 || x == 2 || x==4){
                 
             }
