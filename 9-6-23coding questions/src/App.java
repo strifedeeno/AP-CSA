b21f722fc2bbd608fb1c574be6a3e2a4052a3c08
strifedeeno <coolarnay@gmail.com>
2023-09-06 21:15:01 +0000

    Wednesday, September 6, 2023 at 2:15:01 PM Pacific Daylight Time

--- 9-6-23coding questions/src/App.java
+++ 9-6-23coding questions/src/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         for(int x=0;x<=10;x++){
             if (x==1 || x == 3 || x==5){
-                
+                F
             }
         }
     }
