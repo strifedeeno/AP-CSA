cc159dace8e3046f90453fa5e5a697752353ca6c
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-09-07 05:12:26 +0000

    Wednesday, September 6, 2023 at 10:12:26 PM Pacific Daylight Time

--- 9-6-23coding questions/src/App.java
+++ 9-6-23coding questions/src/App.java
@@ -9,7 +9,7 @@ public class App {
             }
             System.out.println();
             if(x==1||x==3||x==5){
-        for(int z=6-number;z>0;z--){
+    for(int z=6-number;z>0;z--){
                         System.out.print("*");
                     }
                     System.out.println();
