ad6250fa4405898cfb289af72ac0f5ca8868a8c8
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-09-07 05:12:35 +0000

    Wednesday, September 6, 2023 at 10:12:35 PM Pacific Daylight Time

--- 9-6-23coding questions/src/App.java
+++ 9-6-23coding questions/src/App.java
@@ -12,7 +12,7 @@ public class App {
                 for(int z=6-number;z>0;z--){
                     System.out.print("*");
                 }
-    System.out.println();
+System.out.println();
                 }
                 
             
