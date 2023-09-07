6dfd27e6f6164bdf67433f5893c9d0867c95f63c
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-09-07 05:08:35 +0000

    Wednesday, September 6, 2023 at 10:08:35 PM Pacific Daylight Time

--- 9-6-23coding questions/src/App.java
+++ 9-6-23coding questions/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         for(int x=0;x<=10;x++){
-            for(int y=6;y>=0;y--){
+            
                 if (x==0 || x == 2 || x==4){
                     for(int z=x+1;z>0;z--){
                         System.out.print(x);
@@ -16,7 +16,7 @@ public class App {
                     System.out.println();
                 }
                 
-            }
+            
         }
     }
 }
