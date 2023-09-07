19bd2429378f13a93a68aaf57da72a6c6ad70619
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-09-07 05:11:55 +0000

    Wednesday, September 6, 2023 at 10:11:55 PM Pacific Daylight Time

--- 9-6-23coding questions/src/App.java
+++ 9-6-23coding questions/src/App.java
@@ -3,7 +3,7 @@ public class App {
         for(int x=0;x<=10;x++){
             int number=x;
             if (x==0 || x == 2 || x==4){
-                   for(int z=6-x;z>0;z--){
+                  for(int z=6-x;z>0;z--){
                         System.out.print(6-number);
                     }
                     
