9ac813fe9e180108a71d442f9c24fb4715026463
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-09-07 05:47:57 +0000

    Wednesday, September 6, 2023 at 10:47:57 PM Pacific Daylight Time

--- 9-6-23coding questions/src/App.java
+++ 9-6-23coding questions/src/App.java
@@ -18,7 +18,7 @@ public class App {
         System.out.println();
         System.out.println();
         System.out.println();
-        for(int x = -1;x<7;x++){
+        for(int x = 1;x<7;x++){
             
         }
     }
