fad8a5b1f1a8c4980df4d1c16bb97aa639c6a370
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-09-07 05:55:07 +0000

    Wednesday, September 6, 2023 at 10:55:07 PM Pacific Daylight Time

--- 9-6-23coding questions/src/App.java
+++ 9-6-23coding questions/src/App.java
@@ -19,7 +19,7 @@ public class App {
         System.out.println();
         System.out.println();
         for(int i=0;i<4;i++){
-            for(int j=0;j<i;j+=2){
+            for(int j=0;j<i+1;j+=2){
                 System.out.print("*");
             }
             System.out.println();
