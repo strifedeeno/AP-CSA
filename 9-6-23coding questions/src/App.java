cad1585f8cf0224ac679e035261ce1fa80ac8b8e
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-09-07 05:12:15 +0000

    Wednesday, September 6, 2023 at 10:12:15 PM Pacific Daylight Time

--- 9-6-23coding questions/src/App.java
+++ 9-6-23coding questions/src/App.java
@@ -5,8 +5,7 @@ public class App {
             if (x==0 || x == 2 || x==4){
                 for(int z=6-x;z>0;z--){
                     System.out.print(6-number);
-                }    
-}
+                }    }
                 System.out.println();
                 if(x==1||x==3||x==5){
                     for(int z=6-number;z>0;z--){
