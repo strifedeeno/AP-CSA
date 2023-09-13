46a391f35be09779d47e0f9569d04e88e3bdc7de
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-09-13 23:59:05 +0000

    Wednesday, September 13, 2023 at 4:59:05 PM Pacific Daylight Time

--- 3.04 Assignment/src/App.java
+++ 3.04 Assignment/src/App.java
@@ -5,7 +5,7 @@ public class App {
         System.out.println(area);
     }
     public static void distance(int x1,int y1, int x2, int y2){
-
+        
     }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
