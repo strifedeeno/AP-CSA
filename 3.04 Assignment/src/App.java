2ccc035f7036d879b2ba69355fd2dc5f8e06283c
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-09-13 23:56:42 +0000

    Wednesday, September 13, 2023 at 4:56:42 PM Pacific Daylight Time

--- 3.04 Assignment/src/App.java
+++ 3.04 Assignment/src/App.java
@@ -1,6 +1,7 @@
+import java.lang.Math;
 public class App {
-    public static void roadTrip(int diamater){
-        int 
+    public static void roadTrip(Long diameter){
+        double area= diameter * Math.PI;
     }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
