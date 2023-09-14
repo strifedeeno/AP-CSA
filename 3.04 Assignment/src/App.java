d48443420ac827d5189bc1f3d65b23aecd8f188b
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-09-14 00:26:12 +0000

    Wednesday, September 13, 2023 at 5:26:12 PM Pacific Daylight Time

--- 3.04 Assignment/src/App.java
+++ 3.04 Assignment/src/App.java
@@ -23,7 +23,7 @@ public class App {
         double distance2= distance(6.0,19.0,22.0,7.0);
         System.out.println("Distance 2: "+ distance2);
         double distance3= distance(25.0,24.0,16.0,14.0);
-        System.out.println(distance3);
+        System.out.println("Distance 3: "+ distance3);
         double totalDistance= totalTrip(29.0,16.0,34.0,8.0,6.0,19.0);
         System.out.println(totalDistance);
     }
