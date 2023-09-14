74f872672ceae80850a8e06f145a4c4a23da92d0
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-09-14 00:00:44 +0000

    Wednesday, September 13, 2023 at 5:00:44 PM Pacific Daylight Time

--- 3.04 Assignment/src/App.java
+++ 3.04 Assignment/src/App.java
@@ -4,8 +4,8 @@ public class App {
         double area= diameter * Math.PI;
         System.out.println(area);
     }
-    public static void distance(int x1,int y1, int x2, int y2){
-        
+    public static void distance(Double x1,Double y1, Double x2, Double y2){
+        Double distance = Math.sqrt((x2-x1));
     }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
