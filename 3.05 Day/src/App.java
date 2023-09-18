3e8f6133e2a5f6fd942fa17e208cd549c979c5d8
strifedeeno <coolarnay@gmail.com>
2023-09-18 18:16:48 +0000

    Monday, September 18, 2023 at 8:16:48 PM Central European Summer Time

--- 3.05 Day/src/App.java
+++ 3.05 Day/src/App.java
@@ -1,14 +1,4 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        String z="lol";
-
-int a =6;
-for(int x=5;x<12;x+=5){
-      for (int y=4; y<6;y++){
-      a-=4;
-     z+=3+4;
-      }
-   }
-System.out.print(z+a);
-    }
-}
+        System.out.println("YAY!");
+    }}
