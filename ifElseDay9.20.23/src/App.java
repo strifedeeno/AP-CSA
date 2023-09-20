de226a7511b70eba488f8864439adf7feaca6f6a
strifedeeno <coolarnay@gmail.com>
2023-09-20 21:12:03 +0000

    Wednesday, September 20, 2023 at 11:12:03 PM Central European Summer Time

--- ifElseDay9.20.23/src/App.java
+++ ifElseDay9.20.23/src/App.java
@@ -1,24 +1,6 @@
 import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner console = new Scanner(System.in);
-        System.out.print("What grade did you earn : ");
-        double grade = console.nextDouble();
-        if(grade>=4.0){
-            System.out.println("You got a A!");
-        }
-        else{
-            if(grade>=3.0){
-                System.out.println("You got a B!");
-            }
-            else{
-                if(grade>=2.0){
-                    System.out.println("You got a C!");
-                }
-                else{
-                    System.out.println("You got a D or F!");
-                }
-            }
-        }
+        
     }
 }
