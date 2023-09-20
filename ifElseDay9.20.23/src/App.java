32df581b11cd3cd7cba9d950d6e152a98be575e3
strifedeeno <coolarnay@gmail.com>
2023-09-20 20:50:42 +0000

    Wednesday, September 20, 2023 at 10:50:42 PM Central European Summer Time

--- ifElseDay9.20.23/src/App.java
+++ ifElseDay9.20.23/src/App.java
@@ -2,6 +2,6 @@ import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner console = new Scanner(System.in);
-        sout
+        System.out.println();
     }
 }
