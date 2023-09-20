a40e4be5ccb9a1b21913c9ca756eb15bc138d328
strifedeeno <coolarnay@gmail.com>
2023-09-20 21:34:08 +0000

    Wednesday, September 20, 2023 at 11:34:08 PM Central European Summer Time

--- ifElseDay9.20.23/src/App.java
+++ ifElseDay9.20.23/src/App.java
@@ -6,7 +6,7 @@ public class App {
     public static void extraStart(String sentence){
             String charecter3= sentence.substring(0,3);
             System.out.print(charecter3);
-            System.out.println(charecter3.toUpperCase());
-            
+            System.out.print(charecter3.toUpperCase());
+            System.out.print(charecter3.toLowerCase());
         }
 }
