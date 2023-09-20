262bd94b88489654b40e11bede88bb97f15788d6
strifedeeno <coolarnay@gmail.com>
2023-09-20 20:50:55 +0000

    Wednesday, September 20, 2023 at 10:50:55 PM Central European Summer Time

--- ifElseDay9.20.23/src/App.java
+++ ifElseDay9.20.23/src/App.java
@@ -2,6 +2,6 @@ import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner console = new Scanner(System.in);
-        System.out.print("ha");
+        System.out.print("What grade did ");
     }
 }
