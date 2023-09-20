692cf9e386d12b794921af21a53c2516dc0768bf
strifedeeno <coolarnay@gmail.com>
2023-09-20 21:01:15 +0000

    Wednesday, September 20, 2023 at 11:01:15 PM Central European Summer Time

--- ifElseDay9.20.23/src/App.java
+++ ifElseDay9.20.23/src/App.java
@@ -4,8 +4,21 @@ public class App {
         Scanner console = new Scanner(System.in);
         System.out.print("What grade did you earn : ");
         double grade = console.nextDouble();
-        if(grade>=2.0){
-            
+        if(grade>=4.0){
+            System.out.println("You got a A!");
+        }
+        else{
+            if(grade>=3.0){
+                System.out.println("You got a B!");
+            }
+            else{
+                if(grade>=2.0){
+                    System.out.println("You got a C!");
+                }
+                else{
+                    System.out.println("You got a D or F!");
+                }
+            }
         }
     }
 }
