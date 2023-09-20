c63251fd3259da461910b5ee656822d2f45a6c83
strifedeeno <coolarnay@gmail.com>
2023-09-20 21:47:23 +0000

    Wednesday, September 20, 2023 at 11:47:23 PM Central European Summer Time

--- ifElseDay9.20.23/src/App.java
+++ ifElseDay9.20.23/src/App.java
@@ -6,13 +6,23 @@ public class App {
         System.out.println(extraStart("123"));
         System.out.println(extraStart("GRAPE"));
         System.out.println();
+        System.out.println("exercise 2 - nFromTheMiddle");
+        System.out.println(nFromTheMiddle("go", 1));
+        System.out.println(nFromTheMiddle("I like ice", 4));
+        System.out.println(nFromTheMiddle("Phrobots", 3));
+        System.out.println();
     }
     public static String extraStart(String sentence){
             String charecter3= sentence.substring(0,3);
             return charecter3+charecter3.toUpperCase()+charecter3.toLowerCase();
     }
-    public static String nFromTheMiddle(String sentence){
-            String charecter3= sentence.substring(0,3);
-            return charecter3+charecter3.toUpperCase()+charecter3.toLowerCase();
+    public static String nFromTheMiddle(String input,int number){
+        if(input.length()>=2){
+            String subStr= input.substring(0,number);
+            return subStr;
+        }
+        else{
+            return "The input is less than 2 charecters.";
+        }
     }
-}
+    }
