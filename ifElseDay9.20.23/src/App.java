1fbbdf225d769a029ef133e69651bf3f282ee28b
strifedeeno <coolarnay@gmail.com>
2023-09-20 22:17:28 +0000

    Thursday, September 21, 2023 at 12:17:28 AM Central European Summer Time

--- ifElseDay9.20.23/src/App.java
+++ ifElseDay9.20.23/src/App.java
@@ -18,7 +18,7 @@ public class App {
     }
     public static String nFromTheMiddle(String input,int number){
         if(input.length()>=2 && 1<=number && number<=(input.length()/2)){
-            String subStr= input.substring(number, input.length()/);
+            String subStr= input.substring(number, input.length()-2);
             return subStr;
         }
         else{
