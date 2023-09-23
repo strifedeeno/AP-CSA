d63ebfe29c349d1fa1cf5bca797bc1f087fb103b
strifedeeno <coolarnay@gmail.com>
2023-09-23 04:54:12 +0000

    Saturday, September 23, 2023 at 6:54:12 AM Central European Summer Time

--- ifElseDay9.20.23/src/App.java
+++ ifElseDay9.20.23/src/App.java
@@ -82,7 +82,7 @@ System.out.println();
         input=input.toLowerCase();
         int lastnum = input.indexOf("like");
         if(input.charAt(0)=='i'){
-            return "Here are some"+ input.substring(lastnum);
+            return "Here are some"+ input.substring(lastnum+4);
         }
         return "This is a string";
     }
