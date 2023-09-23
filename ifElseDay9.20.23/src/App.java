c9da4f86e1907176fc7c4948d0c0bcdefeecc8fb
strifedeeno <coolarnay@gmail.com>
2023-09-23 05:30:59 +0000

    Saturday, September 23, 2023 at 7:30:59 AM Central European Summer Time

--- ifElseDay9.20.23/src/App.java
+++ ifElseDay9.20.23/src/App.java
@@ -96,16 +96,16 @@ System.out.println();
         return "This is a string";
     }
     public static String codeMaker(String input){
-        public static String codeMaker(String input) {
-            char firstChar = input.charAt(0);
-            char lastChar = input.charAt(input.length() - 1);
-            
-            String middleSection = input.substring(2, 3) + firstChar + input.substring(3, input.length() - 1) + "a";
-            String newString = input.substring(1, 2) + lastChar + middleSection + lastChar + "y";
-            
-            return newString;
-        }
-        
+public static String codeMaker(String input) {
+    char firstChar = input.charAt(0);
+    char lastChar = input.charAt(input.length() - 1);
+    
+    String middleSection = input.substring(2, 3) + firstChar + input.substring(3, input.length() - 1) + "a";
+    String newString = input.substring(1, 2) + lastChar + middleSection + lastChar + "y";
+    
+    return newString;
+}
+
     }
 
 }
