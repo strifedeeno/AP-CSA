2b1c6793c0df6ed02dcee7d5588de85d164c34f0
strifedeeno <coolarnay@gmail.com>
2023-09-23 05:00:55 +0000

    Saturday, September 23, 2023 at 7:00:55 AM Central European Summer Time

--- ifElseDay9.20.23/src/App.java
+++ ifElseDay9.20.23/src/App.java
@@ -96,7 +96,7 @@ System.out.println();
         return "This is a string";
     }
     public static String codeMaker(String input){
-    
+
         String newString = input.substring(0, 0) + input.substring(0 + 1);
         return newString;
     }
