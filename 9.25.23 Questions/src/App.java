1e0d4b4ec07eca810e17c5553b94646c5589a7d5
strifedeeno <coolarnay@gmail.com>
2023-09-25 21:48:55 +0000

    Monday, September 25, 2023 at 11:48:55 PM Central European Summer Time

--- 9.25.23 Questions/src/App.java
+++ 9.25.23 Questions/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Please make sure that the first number is smaller than the second number.");
-        System.out.println(cumsum(2,5));
+        System.out.println(cumsum(2,6));
     }
     public static int cumsum(int a, int b){
         int number = 0;
