4365c3a993cce58387e6dff40d752b621d628f0f
strifedeeno <coolarnay@gmail.com>
2023-09-25 22:01:36 +0000

    Tuesday, September 26, 2023 at 12:01:36 AM Central European Summer Time

--- 9.25.23 Questions/src/App.java
+++ 9.25.23 Questions/src/App.java
@@ -3,7 +3,7 @@ import java.math.BigInteger;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Please make sure that the first number is smaller than the second number.");
-        System.out.println(cumsum(2,100));
+        System.out.println(cumsum(2,10000));
     }
     public static BigInteger cumsum(int a, int b) {
         BigInteger number = BigInteger.valueOf(1);
