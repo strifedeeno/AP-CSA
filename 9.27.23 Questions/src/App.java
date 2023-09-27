dca2130af4fdf6f8a469da68bf1de4f6673f6f82
strifedeeno <coolarnay@gmail.com>
2023-09-27 20:53:22 +0000

    Wednesday, September 27, 2023 at 10:53:22 PM Central European Summer Time

--- 9.27.23 Questions/src/App.java
+++ 9.27.23 Questions/src/App.java
@@ -2,5 +2,14 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
-    public s
+    public static int ncounter(String word){
+        int count =0;
+        word=word.toLowerCase();
+        for(int i= word.length(); i>=0;i--){
+            if(word.charAt(i)=='n'){
+                count++;
+            }
+        }
+        return count;
+    }
 }
