345e6effc56b6f80250b20d361c6b563ec00ecc1
strifedeeno <coolarnay@gmail.com>
2023-09-30 04:45:59 +0000

    Saturday, September 30, 2023 at 6:45:59 AM Central European Summer Time

--- 9.29.23 class/src/App.java
+++ 9.29.23 class/src/App.java
@@ -1,11 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        allDigits(10);
     }
     public static void allDigits(int x)  {
         while(x>9){
             System.out.println(x%10);
-            x=x/10
+            x=x/10;
         }
    	}
 
