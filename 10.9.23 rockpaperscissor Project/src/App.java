be7fa6bd401bc1328735a69041408bad3a2ff7c8
strifedeeno <coolarnay@gmail.com>
2023-10-09 21:47:29 +0000

    Monday, October 9, 2023 at 11:47:29 PM Central European Summer Time

--- 10.9.23 rockpaperscissor Project/src/App.java
+++ 10.9.23 rockpaperscissor Project/src/App.java
@@ -5,7 +5,7 @@ public class App {
         Random rand= new Random();
         int randomNumber= rand.nextInt(3)+1;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("");
+        System.out.println("Please enter your pick (1--rock, 2--paper, 3--scissor, q--exit): ");
         String input = scanner.nextLine();
         
     }
