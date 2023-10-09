cdfd12d1047b390d89b8718aa96faab4e595ecf1
strifedeeno <coolarnay@gmail.com>
2023-10-09 21:42:08 +0000

    Monday, October 9, 2023 at 11:42:08 PM Central European Summer Time

--- 10.9.23 rockpaperscissor Project/src/App.java
+++ 10.9.23 rockpaperscissor Project/src/App.java
@@ -2,7 +2,6 @@ import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
         Random rand= new Random();
-        System.out.print("hi");
         int randomNumber= rand.nextInt(3);
         System.out.println(randomNumber);
     }
