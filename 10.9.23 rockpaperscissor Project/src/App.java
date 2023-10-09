884c25ad3b2f4eabda37f7ae161d8858f9d0cfe8
strifedeeno <coolarnay@gmail.com>
2023-10-09 22:08:22 +0000

    Tuesday, October 10, 2023 at 12:08:22 AM Central European Summer Time

--- 10.9.23 rockpaperscissor Project/src/App.java
+++ 10.9.23 rockpaperscissor Project/src/App.java
@@ -7,11 +7,15 @@ public class App {
         System.out.println("Please enter your pick (1--rock, 2--paper, 3--scissor, q--exit): ");
         String input = scanner.nextLine();
         System.out.println("Computer picks: "+ randomNumber);
+        int numinput=0;
         if(input.equals("q")){
             return;
         }
         else{
-            int numinput = Integer.valueOf(input);
+            numinput = Integer.valueOf(input);
+        }
+        if(numinput==3 && randomNumber==2){
+
         }
     }
 }
