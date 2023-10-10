5945d1ccda8f734bb30edd2df9ed93d36abad3fe
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-10-10 05:56:33 +0000

    Monday, October 9, 2023 at 10:56:33 PM Pacific Daylight Time

--- 10.9.23 rockpaperscissor Project/src/App.java
+++ 10.9.23 rockpaperscissor Project/src/App.java
@@ -49,7 +49,7 @@ public class App {
                 System.out.println("You win.");
                 wincount++;
             }
-            System.out.println(Youwincount);
+            System.out.println(""wincount);
         }
     }
 }
