1c922108e1d3ef94aca6145dc1397787d44dc490
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-10-10 05:58:12 +0000

    Monday, October 9, 2023 at 10:58:12 PM Pacific Daylight Time

--- 10.9.23 rockpaperscissor Project/src/App.java
+++ 10.9.23 rockpaperscissor Project/src/App.java
@@ -11,7 +11,6 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter your pick (1--rock, 2--paper, 3--scissor, q--exit): ");
         String input = scanner.nextLine();
-
         
             if(input.equals("q")){
                 break;
