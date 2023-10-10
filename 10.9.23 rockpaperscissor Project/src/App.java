70011be8dda7dc2218c54b830a95b0db29de9968
strifedeeno <coolarnay@gmail.com>
2023-10-10 05:53:27 +0000

    Tuesday, October 10, 2023 at 7:53:27 AM Central European Summer Time

---
+++ 10.9.23 rockpaperscissor Project/src/App.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int wincount=0;
+        Random rand= new Random();
+        int randomNumber= rand.nextInt(3)+1;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your pick (1--rock, 2--paper, 3--scissor, q--exit): ");
+        String input = scanner.nextLine();
+        System.out.println("Computer picks: "+ randomNumber);
+        int numinput=0;
+        boolean thing=true;
+        while (thing=true) {
+            if(input.equals("q")){
+                break;
+            }
+            else{
+                numinput = Integer.valueOf(input);
+            }
+            if(numinput==3 && randomNumber==2){
+                System.out.println("You won.");
+                wincount++;
+            }
+            else if(numinput==3 && randomNumber==1){
+                System.out.println("You lost.");
+            }
+            else if(numinput==3 && randomNumber==3){
+                System.out.println("You tied");
+            }
+            else if(numinput==2 && randomNumber==1){
+                System.out.println("You won.");
+                wincount++;
+            }
+            else if(numinput==2 && randomNumber==2){
+                System.out.println("You tied");
+            }
+            else if(numinput==2 && randomNumber==3){
+                System.out.println("You lost.");
+            }
+            else if(numinput==1 && randomNumber==1){
+                System.out.println("You tied");
+            }
+            else if(numinput==1 && randomNumber==2){
+                System.out.println("You lose.");
+            }
+            else if(numinput==1 && randomNumber==3){
+                System.out.println("You win.");
+            }
+        }
+    }
+}
