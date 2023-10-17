cb6a69be7fe9b7ddee5d26d7a6fdf1f84f2f36c6
Arn B <arnay@Arns-MacBook-Pro.local>
2023-10-17 10:48:55 -0700

    hello how are you

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,14 +1,15 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        for(int x = 1; x<3;x++){
-            for (int y = 2; y<5;y++){
-                for(int z = 3; z<4;z++){
-                    System.out.print(y);}}}
-
-
-
-
-
-
-
-    }}
+        Scanner scanner = new Scanner(System.in);
+        int[] myarray = new int[499];
+        int even=0;
+        for(int d=0; d <myarray.length; d++) {
+            myarray[d] = even;
+            even+=2;
+        }
+        for(int i = 278; i<=456; i++) {
+            System.out.println(myarray[i]);
+        }
+    }
+}
