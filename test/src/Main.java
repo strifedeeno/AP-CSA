5aae5edd606f455f0bb871800dc5d2f88f60e84b
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-10-22 18:13:48 +0000

    Sunday, October 22, 2023 at 11:13:48 AM Pacific Daylight Time

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,15 +1,19 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        String[] ar1= {"dog", "a", "howo"};
-        int[] thing= lengther(ar1);
-        System.out.println(Arrays.toString(thing));
+        String[] ar1={"dog", "a", "howo"};
+        System.out.println(voweler(ar1));
     }
-    public static int[] lengther(String[] thing){
-        int[] intarrya = new int[thing.length];
-        for(int i=0; i<thing.length; i++){
-            intarrya[i] = thing[i].length();
+    public static int voweler(String[] thing){
+        int count = 0;
+        for(int i = 0; i <thing.length; i++){
+            for(int j = 0; j < thing[i].length(); j++){
+                if (thing[i].charAt(j) == 'o'){
+                    count++;
+                }
+            }
+            
         }
-        return intarrya;
+        return count;
     }
 }
