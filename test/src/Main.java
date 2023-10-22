8e090f52d5ce807f8b1185fafa3d56f62451a05a
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-10-22 00:59:29 +0000

    Saturday, October 21, 2023 at 5:59:29 PM Pacific Daylight Time

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,15 +1,6 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int[] myarray = new int[499];
-        int even=0;
-        for(int d=0; d <myarray.length; d++) {
-            myarray[d] = even;
-            even+=2;
-        }
-        for(int i = 278; i<=456; i++) {
-            System.out.println(myarray[i]);
-        }
+        
     }
 }
