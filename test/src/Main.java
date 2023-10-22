4717cf94d8f5d1f3a47177382f1a8ba4f09bff5f
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-10-22 00:59:16 +0000

    Saturday, October 21, 2023 at 5:59:16 PM Pacific Daylight Time

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,23 +1,5 @@
 import java.util.*;
 public class Main {
-<<<<<<< HEAD
-    public static void main(String[] args){
-int x=4;
-int y=7;
-int z=9;
-        z=mystery(y,x,z);
-        y=mystery(z,x,y);
-        x=mystery(x,z,y);
-        System.out.println(x);
-    }
-public static int mystery(int x,int y,int z){
-        x++;
-        y--;
-        z=x-y;
-        return z;
-}
-}
-=======
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] myarray = new int[499];
@@ -31,4 +13,3 @@ public static int mystery(int x,int y,int z){
         }
     }
 }
->>>>>>> cb6a69be7fe9b7ddee5d26d7a6fdf1f84f2f36c6
