2daa613ec6c29ecdbd8a5216a0346381b3576e7d
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-10-22 01:24:50 +0000

    Saturday, October 21, 2023 at 6:24:50 PM Pacific Daylight Time

--- test/src/Main.java
+++ test/src/Main.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         int[] ar1= {1,-3,-5,9,0,-2};
+        neg(ar1);
         System.out.println(Arrays.toString(ar1));
     }
     public static int[] neg(int[] thing){
