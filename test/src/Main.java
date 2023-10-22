d389fc404d2ddaf4f3b14573b49d3ae8521155e5
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-10-22 01:38:23 +0000

    Saturday, October 21, 2023 at 6:38:23 PM Pacific Daylight Time

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,16 +1,15 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        int[] ar1= {"dog", "a", "howo"};
-        neg(ar1);
+        String[] ar1= {"dog", "a", "howo"};
+        lengther(ar1);
         System.out.println(Arrays.toString(ar1));
     }
-    public static int[] neg(int[] thing){
+    public static int[] lengther(String[] thing){
+        int[] intarrya = new int[thing.length];
         for(int i=0; i<thing.length; i++){
-            if(thing[i]>0){
-                thing[i]=0;
-            }
-        };
-        return thing;
+            intarrya[i] = thing[i].length();
+        }
+        return intarrya;
     }
 }
