8ee9ac825a98640b363e8d23a4addd21628d0d74
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-10-22 18:17:10 +0000

    Sunday, October 22, 2023 at 11:17:10 AM Pacific Daylight Time

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,19 +1,15 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        String[] ar1={"a","dog", "a", "howo","a"};
-        System.out.println(counter(ar1,"a"));
+        String[] ar1= {"dog", "a", "howo"};
+        int[] thing= lengther(ar1);
+        System.out.println(Arrays.toString(thing));
     }
-    public static int counter(String[] thing, String target){
-        int count = 0;
-        for(int i = 0; i <thing.length; i++){
-            for(int j = 0; j < thing[i].length(); j++){
-                if (thing[i].contains(target) == true){
-                    count++;
-                }
-            }
-            
+    public static int[] lengther(String[] thing){
+        int[] intarrya = new int[thing.length];
+        for(int i=0; i<thing.length; i++){
+            intarrya[i] = thing[i].length();
         }
-        return count;
+        return intarrya;
     }
 }
