e7ba1104c0740a3f7e89296463fd9375cfbd159e
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-10-22 01:17:05 +0000

    Saturday, October 21, 2023 at 6:17:05 PM Pacific Daylight Time

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,6 +1,19 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        
+        String[] ar1={"dog", "a", "howo"};
+        voweler(ar1);
+    }
+    public static int voweler(String[] thing){
+        int count = 0;
+        for(int i = 0; i <thing.length; i++){
+            for(int j = 0; j < thing[i].length(); j++){
+                if (thing[i].charAt(j) == 'o'){
+                    count++;
+                }
+            }
+            
+        }
+        return count;
     }
 }
