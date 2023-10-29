e006db34eb577066dbf9dfc6a2bf27eabdddbab2
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-10-29 07:47:13 +0000

    Sunday, October 29, 2023 at 12:47:13 AM Pacific Daylight Time

--- unit_4.06_mini_project/src/App.java
+++ unit_4.06_mini_project/src/App.java
@@ -7,7 +7,7 @@ public class App {
     public static void magicBox(int x){
         int[][] array= new int[x*2+1][x*2+1];
         for(int i =0; i<array.length;i++){
-
+            
         }
         for(int i =0;i<array.length;i++){
             System.out.println(Arrays.toString(array[i]));
