2b33d341338bbc56b5ed96ccd49bd3efc280c358
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-10-29 08:28:04 +0000

    Sunday, October 29, 2023 at 1:28:04 AM Pacific Daylight Time

--- unit_4.06_mini_project/src/App.java
+++ unit_4.06_mini_project/src/App.java
@@ -7,15 +7,16 @@ public class App {
 
     public static void magicBox(int x) {
         int[][] array = new int[x * 2 + 1][x * 2 + 1];
-        for(int num=x;num>=0;num--){
-        for (int i = array.length-1; i >0; i--) {
-            for (int j = array.length-1; j > 0; j--) {
-                if (i == num || j == num || i == array.length - num || j == array.length - num) {
-                    array[i][j] = num;
+        for (int num = x; num >= 0; num--) {
+            for (int i = array.length - 1; i > 0; i--) {
+                for (int j = array.length - 1; j > 0; j--) {
+                    if (i == num || j == num || i == array.length - num || j == array.length - num) {
+                        array[i][j] = num;
+                    }
                 }
+
             }
-            
-        }}
+        }
         for (int i = 0; i < array.length; i++) {
             System.out.println(Arrays.toString(array[i]));
         }
