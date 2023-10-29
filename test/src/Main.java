801647022633f2fe3c81a74dce2c743ac632324c
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-10-29 19:06:31 +0000

    Sunday, October 29, 2023 at 12:06:31 PM Pacific Daylight Time

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,26 +1,26 @@
 import java.util.*;
 class Main {
-  public static void main(String[] args) {
-    magicBox(3);
-  }
+    public static void main(String[] args) {
+        magicBox(3);
+    }
 
-  public static void magicBox(int x)
-  {
-    int[][] box = new int[x*2+1][x*2+1];
-    for(int i = 0; i < box.length-1; i++)
-      {
-      for(int j =0; j<box.length-1; i++)
-        {
-          if(i == 0 || j == 0 || i == x*2|| j == x*2)
-          {
-            box[i][j] = 1;
-          }
-            
-        }
-      }
-    for(int i = 0; i < box.length-1; i++)
-      {
-        System.out.println(Arrays.toString(box[i]));
-      }
-  }
+    public static void magicBox(int x)
+    {
+        int[][] box = new int[x*2+1][x*2+1];
+        for(int i = 0; i < box.length-1; i++)
+            {
+            for(int j =0; j<box.length-1; j++) // fixed the loop increment
+                {
+                    if(i == 0 || j == 0 || i == x*2|| j == x*2)
+                    {
+                        box[i][j] = 1;
+                    }
+                        
+                }
+            }
+        for(int i = 0; i < box.length-1; i++)
+            {
+                System.out.println(Arrays.toString(box[i]));
+            }
+    }
 }
