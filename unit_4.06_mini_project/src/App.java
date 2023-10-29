4b228c79f601b75c351a36965b4c2847eda017b6
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-10-29 08:31:56 +0000

    Sunday, October 29, 2023 at 1:31:56 AM Pacific Daylight Time

--- unit_4.06_mini_project/src/App.java
+++ unit_4.06_mini_project/src/App.java
@@ -21,4 +21,20 @@ public class App {
             System.out.println(Arrays.toString(array[i]));
         }
     }
+     public static void pyramid(int x) {
+        int[][] array = new int[x * 2 + 1][x * 2 + 1];
+        for (int num = x; num >= 0; num--) {
+            for (int i = array.length - 1; i > 0; i--) {
+                for (int j = array.length - 1; j > 0; j--) {
+                    if (i == num || j == num || i == array.length - num-1 || j == array.length - num-1) {
+                        array[i][j] = num;
+                    }
+                }
+
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(Arrays.toString(array[i]));
+        }
+    }
 }
