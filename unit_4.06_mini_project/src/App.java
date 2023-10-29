d7c7a5e8824691731fa0003f4d26df0be8b23aa9
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-10-29 08:32:16 +0000

    Sunday, October 29, 2023 at 1:32:16 AM Pacific Daylight Time

--- unit_4.06_mini_project/src/App.java
+++ unit_4.06_mini_project/src/App.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class App {
     public static void main(String[] args) throws Exception {
         magicBox(2);
-        pyramid(3);
+        pyramid(6);
     }
 
     public static void magicBox(int x) {
