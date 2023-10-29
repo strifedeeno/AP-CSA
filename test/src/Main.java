adb57bb1bd1dd6fbf42dce82bee8f0e1f07ad837
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-10-29 19:05:30 +0000

    Sunday, October 29, 2023 at 12:05:30 PM Pacific Daylight Time

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,26 +1,21 @@
 import java.util.*;
-class Main {
-  public static void main(String[] args) {
-    magicBox(3);
-  }
 
-  public static void magicBox(int x)
-  {
-    int[][] box = new int[x*2+1][x*2+1];
-    for(int i = 0; i < box.length-1; i++)
-      {
-      for(int j =0; j<box.length-1; i++)
-        {
-          if(i == 0 || j == 0 || i == x*2|| j == x*2)
-          {
-            box[i][j] = 1;
-          }
-            
+class Main {
+    public static void main(String[] args) {
+        magicBox(3);
+    }
+
+    public static void magicBox(int x) {
+        int[][] box = new int[x*2+1][x*2+1];
+        for(int i = 0; i < box.length-1; i++) {
+            for(int j = 0; j < box.length-1; j++) {
+                if(i == 0 || j == 0 || i == x*2 || j == x*2) {
+                    box[i][j] = 1;
+                }
+            }
         }
-      }
-    for(int i = 0; i < box.length-1; i++)
-      {
-        System.out.println(Arrays.toString(box[i]));
-      }
-  }
+        for(int i = 0; i < box.length-1; i++) {
+            System.out.println(Arrays.toString(box[i]));
+        }
+    }
 }
