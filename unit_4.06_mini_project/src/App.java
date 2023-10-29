38545fa1a6c6c3bcfc2395510a5684f72471d1a9
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-10-29 07:53:57 +0000

    Sunday, October 29, 2023 at 12:53:57 AM Pacific Daylight Time

--- unit_4.06_mini_project/src/App.java
+++ unit_4.06_mini_project/src/App.java
@@ -8,7 +8,7 @@ public class App {
         int[][] array= new int[x*2+1][x*2+1];
         for(int i =0; i<array.length-1;i++){
             for(int j = 0;j<array[i].length-1;j++){
-                if(i==0||j==0||i==array.length-1||j==array[i].length-1){
+                if(i==0||j==0||i==array.length-1||j==array.length-1){
                     array[i][j] = 2;
                 }
             }
