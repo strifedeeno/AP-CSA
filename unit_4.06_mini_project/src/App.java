3d8e9fb1d36086362d7f9641fbc439e125149255
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-10-29 07:40:56 +0000

    Sunday, October 29, 2023 at 12:40:56 AM Pacific Daylight Time

--- unit_4.06_mini_project/src/App.java
+++ unit_4.06_mini_project/src/App.java
@@ -2,5 +2,5 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
-    public static void 
+    public static void magicBox(int x){}
 }
