0664886376a3e36c2852932b01296a5627e59465
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-10-29 07:57:13 +0000

    Sunday, October 29, 2023 at 12:57:13 AM Pacific Daylight Time

--- unit_4.06_mini_project/src/App.java
+++ unit_4.06_mini_project/src/App.java
@@ -9,6 +9,7 @@ public class App {
         for(int i =0; i<array.length-1;i++){
             for(int j = 0;j<array.length-1;j++){
                 if(i==0||j==0||i==array.length-1||j==array.length-1){
+                    System.out.println(array.length-1);
                     array[i][j] = 2;
                 }
             }
