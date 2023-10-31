c0249ef0160562417441949b402e84f27ef85034
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-10-31 00:02:54 -0700

    stuff

--- unit_4.06_mini_project/src/lines.java
+++ unit_4.06_mini_project/src/lines.java
@@ -2,23 +2,33 @@ import java.util.*;
 public class lines {
     
     public static void main(String[] args) {
-        lines();
+        ArrayList<String> liststring=new ArrayList<String>();
+        liststring= getLines();
+        averageLineLength(liststring);
     }
-    public static ArrayList<String> lines(){
+    public static ArrayList<String> getLines(){
         Scanner scan = new Scanner(System.in);
         boolean thing=true;
         ArrayList<String> liststring=new ArrayList<String>();
         while(thing=true){
-            System.out.println("Enter username");
+            System.out.println("Enter input: ");
             String hi= scan.nextLine();
             if(hi.equals("!go")==true){
-                thing=false;
-                S
+                System.out.println("Stopping");
+                break;
             }
             else{
                 liststring.add(hi);
             }
         }
         return liststring;
+    }
+    public static void averageLineLength(ArrayList<String> thing){
+        int i =0;
+        for (String string : thing) {
+            System.out.println(string+" The length is: "+string.length());
+            i+=string.length();
+        }
+        System.out.println("The average length for all inputs is: "+ i/(thing.size()));
     }
 }
