8caa19afc9db57290299c56c95c40954929b814c
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-10-30 22:56:13 -0700

    idk

--- unit_4.06_mini_project/src/ArrayListPractice.java
+++ unit_4.06_mini_project/src/ArrayListPractice.java
@@ -4,7 +4,14 @@ public class ArrayListPractice {
         ArrayList<String> thing = new ArrayList<String>();
         thing= createList();
         System.out.println(thing);
-        System.out.println(addAdjectives(thing));
+        thing= addAdjectives(thing);
+        System.out.println(thing);
+        thing= updateColor(thing);
+        System.out.println(thing);
+        thing=removeTheCat(thing);
+        System.out.println(thing);
+        ToSentence(thing);
+        System.out.println(sequence(17,11,273));
     }
     public static ArrayList<String> createList(){
         ArrayList<String> thing=new ArrayList<String>();
@@ -19,8 +26,35 @@ public class ArrayListPractice {
         return thing;
     }
     public static ArrayList<String> addAdjectives(ArrayList<String> thing){
-        thing.add(1,"quick");
-        thing.add(2,"red");
+        thing.add(thing.indexOf("fox"),"quick");
+        thing.add(thing.indexOf("fox"),"red");
+        thing.add(thing.indexOf("dog"),"lazy");
         return thing;
+    }
+    public static ArrayList<String> updateColor(ArrayList<String> thing){
+        thing.set(thing.indexOf("red"),"brown");
+        return thing;
+    }
+    public static ArrayList<String> removeTheCat(ArrayList<String> thing){
+        thing.remove("and");
+        thing.remove("cat");
+        return thing;
+    }
+    public static void ToSentence(ArrayList<String> thing){
+        for(int i=0;i<thing.size();i++){
+            if(i<thing.size()-1){
+                System.out.print(thing.get(i)+" ");
+            }
+            else{
+                System.out.println(thing.get(i)+".");
+            }
+        }
+    }
+    public static ArrayList<Integer> sequence(int start,int increase,int max){
+        ArrayList<Integer> numberthing= new ArrayList<Integer>();
+        for(int i=0;i<max;i+=increase){
+            numberthing.add(i);
+        }
+        return numberthing;
     }
 }
