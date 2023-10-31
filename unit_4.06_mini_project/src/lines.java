d08b0d9196780f6ba82c8838bdc5be4a9b69c027
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-10-31 06:42:38 +0000

    Monday, October 30, 2023 at 11:42:38 PM Pacific Daylight Time

--- unit_4.06_mini_project/src/lines.java
+++ unit_4.06_mini_project/src/lines.java
@@ -7,9 +7,10 @@ public class lines {
     public static void lines(){
         Scanner scan = new Scanner(System.in);
         boolean thing=true;
+        
         while(thing=true){
             System.out.println("Enter username");
-            String i= scan.nextLine();
+            scan.nextLine();
         }
     }
 }
