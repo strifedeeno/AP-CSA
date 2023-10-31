c52a069932b21448d7ee11a4de11831d22c03dac
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-10-31 04:53:20 +0000

    Monday, October 30, 2023 at 9:53:20 PM Pacific Daylight Time

--- unit_4.06_mini_project/src/ArrayListPractice.java
+++ unit_4.06_mini_project/src/ArrayListPractice.java
@@ -4,7 +4,7 @@ public class ArrayListPractice {
         createList();
     }
     public static void createList(){
-        ArrayList thing=new ArrayList();
+        ArrayList thing=new ArrayList<String>();
         thing.add("The");
     }
 }
