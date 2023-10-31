feae82dc8f2f5d0b7079eec1358808e230f6a0da
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-10-31 07:13:25 +0000

    Tuesday, October 31, 2023 at 12:13:25 AM Pacific Daylight Time

--- unit_4.06_mini_project/src/lines.java
+++ unit_4.06_mini_project/src/lines.java
@@ -1,44 +1,47 @@
 import java.util.*;
+
 public class lines {
-    
+
     public static void main(String[] args) {
-        ArrayList<String> liststring=new ArrayList<String>();
-        liststring= getLines();
+        ArrayList<String> liststring = new ArrayList<String>();
+        liststring = getLines();
         averageLineLength(liststring);
-        System.out.println("After removing duplicates: "+removeDuplicates(liststring));
+        System.out.println("After removing duplicates: " + removeDuplicates(liststring));
     }
-    public static ArrayList<String> getLines(){
+
+    public static ArrayList<String> getLines() {
         Scanner scan = new Scanner(System.in);
-        boolean thing=true;
-        ArrayList<String> liststring=new ArrayList<String>();
-        while(thing=true){
+        boolean thing = true;
+        ArrayList<String> liststring = new ArrayList<String>();
+        while (thing = true) {
             System.out.println("Enter input: ");
-            String hi= scan.nextLine();
-            if(hi.equals("!go")==true){
+            String hi = scan.nextLine();
+            if (hi.equals("!go") == true) {
                 System.out.println("Stopping");
                 break;
-            }
-            else{
+            } else {
                 liststring.add(hi);
             }
         }
         return liststring;
     }
-    public static void averageLineLength(ArrayList<String> thing){
-        int i =0;
+
+    public static void averageLineLength(ArrayList<String> thing) {
+        int i = 0;
         for (String string : thing) {
-            System.out.println(string+" The length is: "+string.length());
-            i+=string.length();
+            System.out.println(string + " The length is: " + string.length());
+            i += string.length();
         }
-        System.out.println("The average length for all inputs is: "+ i/(thing.size()));
+        System.out.println("The average length for all inputs is: " + i / (thing.size()));
     }
-  public static ArrayList<String> removeDuplicates(ArrayList<String> thing) {
-    ArrayList<String> uniqueList = new ArrayList<String>();
-    for (String s : thing) {
-      if (!uniqueList.contains(s)) {
-        uniqueList.add(s);
-      }
+
+    public static ArrayList<String> removeDuplicates(ArrayList<String> thing) {
+        ArrayList<String> uniqueList = new ArrayList<String>();
+        for (String s : thing) {
+            if (!uniqueList.contains(s)) {
+                uniqueList.add(s);
+            }
+        }
+        return uniqueList;
     }
-    return uniqueList;
-  }
 }
