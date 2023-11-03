5e9edb68a1731284e61f87770e0c522c4a81a420
strifedeeno <coolarnay@gmail.com>
2023-11-03 21:55:10 +0000

    Friday, November 3, 2023 at 2:55:10 PM Pacific Daylight Time

---
+++ tictactoe project/src/tempCodeRunnerFile.java
@@ -0,0 +1,77 @@
+import java.util.*;
+public class App {
+    public static int turn =0;
+    public static void main(String[] args) throws Exception {
+        Scanner scan= new Scanner(System.in);
+        System.out.print("Do you want to play 2 player or 1 player? Input 1 or 2: ");
+        int playermode =scan.nextInt();
+        String[][] board=new String[3][3];
+        if(playermode==1){
+            while(turn<10){
+                winconditions(board);
+                System.out.print("Please input which row you want to go with: ");
+                int movex = scan.nextInt()-1;
+                System.out.print("Please input which column you want to go with: ");
+                int movey = scan.nextInt()-1;
+                if(turn%2==0){
+                    String playerpiece="x";
+                    board[movey][movex]=playerpiece;
+                    printBoard(board);
+                    turn++;
+                }
+                else{
+                    String playerpiece ="o";
+                    board[movey][movex]=playerpiece;
+                    printBoard(board);
+                    turn++;
+                }
+            }
+        }
+        
+        printBoard(board);
+
+    }
+    public static void printBoard(String[][] theboard){
+        for(int i= 0;i<theboard.length;i++){
+            System.out.println(Arrays.toString(theboard[i]));
+        }
+    }
+    public static boolean winconditions(String[][] boardthing){
+        boolean wonornot=false;
+        if(boardthing[0][0]=="x" && boardthing[0][1]=="x" && boardthing[0][2]=="x"){
+            wonornot=true;
+        }else if(boardthing[1][0]=="x" && boardthing[1][1]=="x" && boardthing[1][2]=="x"){
+            wonornot=true;
+        }else if(boardthing[2][0]=="x" && boardthing[2][1]=="x" && boardthing[2][2]=="x"){
+            wonornot=true;
+        }else if(boardthing[0][0]=="x" && boardthing[1][0]=="x" && boardthing[2][0]=="x"){
+            wonornot=true;
+        }else if(boardthing[0][1]=="x" && boardthing[1][1]=="x" && boardthing[2][1]=="x"){
+            wonornot=true;
+        }else if(boardthing[0][2]=="x" && boardthing[1][2]=="x" && boardthing[2][2]=="x"){
+            wonornot=true;
+        }else if(boardthing[0][0]=="x" && boardthing[1][1]=="x" && boardthing[2][2]=="x"){
+            wonornot=true;
+        }else if(boardthing[0][2]=="x" && boardthing[1][1]=="x" && boardthing[2][0]=="x"){
+            wonornot=true;
+        }
+        if(boardthing[0][0]=="o" && boardthing[0][1]=="o" && boardthing[0][2]=="o"){
+            wonornot=true;
+        }else if(boardthing[1][0]=="o" && boardthing[1][1]=="o" && boardthing[1][2]=="o"){
+            wonornot=true;
+        }else if(boardthing[2][0]=="o" && boardthing[2][1]=="o" && boardthing[2][2]=="o"){
+            wonornot=true;
+        }else if(boardthing[0][0]=="o" && boardthing[1][0]=="o" && boardthing[2][0]=="o"){
+            wonornot=true;
+        }else if(boardthing[0][1]=="o" && boardthing[1][1]=="o" && boardthing[2][1]=="o"){
+            wonornot=true;
+        }else if(boardthing[0][2]=="o" && boardthing[1][2]=="o" && boardthing[2][2]=="o"){
+            wonornot=true;
+        }else if(boardthing[0][0]=="o" && boardthing[1][1]=="o" && boardthing[2][2]=="o"){
+            wonornot=true;
+        }else if(boardthing[0][2]=="o" && boardthing[1][1]=="o" && boardthing[2][0]=="o"){
+            wonornot=true;
+        }
+return wonornot;
+    }
+}
