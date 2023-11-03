71b2a7950160274127555df0bd2a3c07cb711036
strifedeeno <coolarnay@gmail.com>
2023-11-03 21:28:27 +0000

    Friday, November 3, 2023 at 2:28:27 PM Pacific Daylight Time

---
+++ tictactoe project/src/App.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class App {
+    public static int turn =0;
+    public static void main(String[] args) throws Exception {
+        Scanner scan= new Scanner(System.in);
+        System.out.print("Do you want to play 2 player or 1 player? Input 1 or 2: ");
+        int playermode =scan.nextInt();
+        String[][] board=new String[3][3];
+        if(playermode==1){
+            while(turn<10){
+                System.out.print("Please input which row you want to go with: ");
+                int movex = scan.nextInt();
+                System.out.print("Please input which column you want to go with: ");
+                int movey = scan.nextInt();
+                if(turn%2==0){
+                    String playerpiece="x";
+                    board[movey][movex]=playerpiece;
+                    printBoard(board);
+                    turn++;
+                }
+                else{
+                    String playerpiece ="o";
+                    board[movey][movex]=playerpiece;
+                    printBoard(board);
+                    turn++;
+                }
+            }
+        }
+        
+        printBoard(board);
+
+    }
+    public static void printBoard(String[][] theboard){
+        for(int i= 0;i<theboard.length;i++){
+            System.out.println(Arrays.toString(theboard[i]));
+        }
+    }
+}
