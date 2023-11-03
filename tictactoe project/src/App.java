8fc77019f0c21ac23102d141116467114894a8b5
strifedeeno <coolarnay@gmail.com>
2023-11-03 21:51:50 +0000

    Friday, November 3, 2023 at 2:51:50 PM Pacific Daylight Time

--- tictactoe project/src/App.java
+++ tictactoe project/src/App.java
@@ -44,7 +44,7 @@ public class App {
             wonornot=true;
         }else if(boardthing[2][0]=="x" && boardthing[2][1]=="x" && boardthing[2][2]=="x"){
             wonornot=true;
-        }else if(boardthing[0][0]=="x" && boardthing[1][0]=="x" && boardthing[2][2]=="x"){
+        }else if(boardthing[0][0]=="x" && boardthing[1][0]=="x" && boardthing[2][0]=="x"){
             wonornot=true;
         }
 return wonornot;
