e7225bfcdcce452ddef878844e24bac999dd4c46
strifedeeno <coolarnay@gmail.com>
2023-11-03 20:59:47 +0000

    Friday, November 3, 2023 at 1:59:47 PM Pacific Daylight Time

--- tictactoe project/src/App.java
+++ tictactoe project/src/App.java
@@ -3,7 +3,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner scan= new Scanner(System.in);
         String[][] board=new String[3][3];
-        
+        printBoard(board);
     }
     public static void printBoard(String[][] theboard){
         for(int i= 0;i<theboard.length;i++){
