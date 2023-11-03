ba0c61bb3c788147f682011c2c77f039be6ffb5c
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-11-03 23:56:08 +0000

    Friday, November 3, 2023 at 4:56:08 PM Pacific Daylight Time

--- tictactoe project/src/App.java
+++ tictactoe project/src/App.java
@@ -60,7 +60,7 @@ public class App {
                     String playerpiece = "o";
                     int random1=rand.nextInt(3);
                     int random2=rand.nextInt(3);
-                    board[rand.nextInt(3)][ran] = playerpiece;
+                    board[rand][random1] = playerpiece;
                     printBoard(board);
                     turn++;
                 }
