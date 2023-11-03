1f036c02a5b66d2783bca6f848e12546f0e8f7bb
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-11-03 23:57:30 +0000

    Friday, November 3, 2023 at 4:57:30 PM Pacific Daylight Time

--- tictactoe project/src/App.java
+++ tictactoe project/src/App.java
@@ -60,7 +60,7 @@ public class App {
                     String playerpiece = "o";
                     int random1 = rand.nextInt(3);
                     int random2 = rand.nextInt(3);
-                    while (board[random2][random1] == null) {
+                    while (board[random2][random1] != null) {
                         board[random2][random1] = playerpiece;
                     }
                     printBoard(board);
