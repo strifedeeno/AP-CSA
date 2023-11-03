67301497bd6d4481b990df1f03dabff596d754c3
strifedeeno <coolarnay@gmail.com>
2023-11-03 21:00:54 +0000

    Friday, November 3, 2023 at 2:00:54 PM Pacific Daylight Time

--- tictactoe project/src/App.java
+++ tictactoe project/src/App.java
@@ -3,8 +3,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner scan= new Scanner(System.in);
         System.out.println("Do you want to play 2 player or 1 player? Input 1 or 2: ");
-        scan.nextInt();
-        
+        inscan.nextInt();
+
         String[][] board=new String[3][3];
         printBoard(board);
 
