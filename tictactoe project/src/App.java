73f1ef8d91982dfbbf3c113de3b993be2d56d433
strifedeeno <77507852+strifedeeno@users.noreply.github.com>
2023-11-03 23:45:00 +0000

    Friday, November 3, 2023 at 4:45:00 PM Pacific Daylight Time

--- tictactoe project/src/App.java
+++ tictactoe project/src/App.java
@@ -42,7 +42,7 @@ public class App {
                     System.out.println("You tied!! Here is the final board:");
                 }
             }
-        }
+        }els
 
         printBoard(board);
 
