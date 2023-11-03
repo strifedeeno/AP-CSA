45b65fe50e2410762aa08c062389b5575428d803
strifedeeno <coolarnay@gmail.com>
2023-11-03 21:02:29 +0000

    Friday, November 3, 2023 at 2:02:29 PM Pacific Daylight Time

--- tictactoe project/src/App.java
+++ tictactoe project/src/App.java
@@ -8,7 +8,7 @@ public class App {
         if(playermode==1){
             boolean game=true;
             while(game==true){
-                System.out.println(Please input);
+                System.out.println("");
             }
         }
         
