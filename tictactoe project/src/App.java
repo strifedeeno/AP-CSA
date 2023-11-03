2d2096f7bc917709cbdfafa53d18952baec5ef4e
strifedeeno <coolarnay@gmail.com>
2023-11-03 21:02:41 +0000

    Friday, November 3, 2023 at 2:02:41 PM Pacific Daylight Time

--- tictactoe project/src/App.java
+++ tictactoe project/src/App.java
@@ -8,7 +8,7 @@ public class App {
         if(playermode==1){
             boolean game=true;
             while(game==true){
-                System.out.println("Please Input where you want to place ");
+                System.out.println("Please Input where you want to place your firs");
             }
         }
         
