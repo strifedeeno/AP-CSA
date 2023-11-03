f3f227108266ca063ccfffa6b2cbec3482fb299f
strifedeeno <coolarnay@gmail.com>
2023-11-03 21:03:27 +0000

    Friday, November 3, 2023 at 2:03:27 PM Pacific Daylight Time

--- tictactoe project/src/App.java
+++ tictactoe project/src/App.java
@@ -10,6 +10,7 @@ public class App {
             while(game==true){
                 System.out.println("Please Input where you want to place your first letter: ");
                 String move = scan.nextLine();
+                
             }
         }
         
